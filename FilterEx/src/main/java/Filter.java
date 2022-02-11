4bb771ef7ce073786b7ce121c8b33d3c3c064dca
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2022-02-11 12:53:47 +0530

    Add files via upload

---
+++ FilterEx/src/main/java/Filter.java
@@ -0,0 +1,60 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class Filter
+ */
+@WebFilter(dispatcherTypes = {DispatcherType.REQUEST }
+					, urlPatterns = { "/Filter" })
+public class Filter implements javax.servlet.Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public Filter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		//chain.doFilter(request, response);
+		
+		  PrintWriter out=response.getWriter(); 
+		    out.print("<script>alert('hello');</script>"); 
+		         
+		    chain.doFilter(request, response);//sends request to next resource 
+		         
+		    out.print("filter is invoked after");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
